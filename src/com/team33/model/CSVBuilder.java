95c68a1fcb23f6e564cba7d029d7909647d0f315
mohamed157 <fm_guerras@esi.dz>
2017-02-18 17:26:06 +0100

    Building the CSVBuilder and CSVFormat

--- src/com/team33/model/CSVBuilder.java
+++ src/com/team33/model/CSVBuilder.java
@@ -1,8 +1,49 @@
 package com.team33.model;
 
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
 /**
  * Created by Amine on 13/02/2017.
  */
 public class CSVBuilder  {
-    CSVFormat csvf;
+    private ArrayList<String> workbooksPaths ;
+    private CSVFormat format;
+    private String destination;
+    private String tempPath;
+
+    public CSVBuilder(ArrayList<String> workbooksPaths, CSVFormat format, String destination) {
+        this.workbooksPaths = workbooksPaths;
+        this.format = format;
+        this.destination = destination;
+    }
+
+    public CSVBuilder(ArrayList<String> workbooksPaths, CSVFormat format) {
+        this.workbooksPaths = workbooksPaths;
+        this.format = format;
+    }
+
+    public CSVBuilder(CSVFormat format) {
+        this.format = format;
+    }
+
+    public void BuildCSV(){
+        tempPath = format.BuildCSV(workbooksPaths);
+    }
+
+    public void ConvertToCSV(){
+        ToCSV toCSV = new ToCSV();
+        try {
+            toCSV.convertExcelToCSV(tempPath,destination);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InvalidFormatException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
 }
