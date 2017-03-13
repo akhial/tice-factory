be55cda20ec8583d4ae2521f961a0aac39b68c62
hamza <fk_zaidi@esi.dz>
2017-03-13 21:30:22 +0100

    Modified StudentFormat added Util class Removed util methodes form UserFormat Addapted TeacherFormat to changes in UserFormat

--- src/com/team33/model/csv/AffectingTeacherToCourseFormat.java
+++ src/com/team33/model/csv/AffectingTeacherToCourseFormat.java
@@ -9,14 +9,7 @@ import java.util.ArrayList;
  * Created by Amine on 13/02/2017.
  */
 public class AffectingTeacherToCourseFormat implements CSVFormat {
-
-    private XSSFWorkbook workbookIn;
-    private XSSFWorkbook workbookOut;
-    private XSSFWorkbook EmailsWorkbook;
-    private Row header = null;
-
-
-
+    
     @Override
     public String buildCSV(ArrayList<String> workbooksPaths) {
         return null;
