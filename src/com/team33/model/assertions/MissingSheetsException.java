a3c2f3e5774718a688774fcced307ea74013c617
mitchell96 <ey_al_houcine@esi.dz>
2017-04-24 19:07:01 +0200

    check doublants et effacements manque qqs trucs

---
+++ src/com/team33/model/assertions/MissingSheetsException.java
@@ -0,0 +1,10 @@
+package com.team33.model.assertions;
+
+/**
+ * Created by Mitchell on 19/04/2017.
+ */
+public class MissingSheetsException extends Exception {
+    public MissingSheetsException (String message){
+        super(message);
+    }
+}
---
+++ src/com/team33/model/assertions/SheetsFoundException.java
@@ -0,0 +1,10 @@
+package com.team33.model.assertions;
+
+/**
+ * Created by Mitchell on 19/04/2017.
+ */
+public class SheetsFoundException extends Exception {
+    public SheetsFoundException(String message){
+        super(message);
+    }
+}
