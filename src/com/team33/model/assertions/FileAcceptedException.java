1f8a089808eaf18838e9ef94fa47ebb6515ca4ec
mitchell96 <ey_al_houcine@esi.dz>
2017-04-24 19:04:37 +0200

    check doublants et effacements manque qqs trucs

---
+++ src/com/team33/model/assertions/FileAcceptedException.java
@@ -0,0 +1,11 @@
+package com.team33.model.assertions;
+
+/**
+ * Created by Mitchell on 18/04/2017.
+ */
+public class FileAcceptedException extends Exception{
+    public FileAcceptedException (String message) {
+        super(message);
+    }
+
+}
--- src/com/team33/model/assertions/InternshipFormatChecker.java
+++ src/com/team33/model/assertions/InternshipFormatChecker.java
@@ -1,5 +1,6 @@
 package com.team33.model.assertions;
 
+import com.sun.xml.internal.org.jvnet.fastinfoset.FastInfosetException;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
@@ -8,6 +9,7 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.*;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 import org.apache.poi.xwpf.extractor.XWPFWordExtractor;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
@@ -19,9 +21,9 @@ import org.apache.poi.xwpf.usermodel.XWPFTableRow;
  * Created by Amine on 14/02/2017.
  */
 public class InternshipFormatChecker implements ExcelFormat {
-    public String ConvertWordTableToExcel(String wordPath){
+    public String ConvertWordTableToExcel(String wordPath) throws IOException{
         String type="Interniship";
-        try {
+
             FileInputStream fileInputStream = new FileInputStream(wordPath);
             XWPFDocument xwpfDocument = new XWPFDocument(fileInputStream);
             XWPFWordExtractor we = new XWPFWordExtractor(xwpfDocument);
@@ -53,37 +55,25 @@ public class InternshipFormatChecker implements ExcelFormat {
             xssfWorkbook.write(fileOutputStream);
             fileInputStream.close();
             return excelName;
-        } catch (FileNotFoundException e1) {
-            e1.printStackTrace();
-        } catch (IOException e1) {
-            e1.printStackTrace();
-        }
-        return "";
+
 
     }
     @Override
 
 
 
-     public boolean checkFormat(File f1){
-        String s=f1.getName();
+     public boolean checkFormat(String f1) throws IOException, MissingFieldsException, FileAcceptedException, NoLineFoundException {
+        String s=f1;
         InternshipFormatChecker wrd=new InternshipFormatChecker();
         String s2=wrd.ConvertWordTableToExcel(s);
         File f2 = new File(s2);
-
-
-
-        try {
-
             XSSFWorkbook wB = new XSSFWorkbook(new FileInputStream(f2));
+        if (wB.getSheetAt(0).getRow(wB.getSheetAt(0).getLastRowNum())==null)
+            throw new NoSuchElementException("Fichier Vide !");
             XSSFSheet sheet = wB.getSheetAt(0);
             Iterator<Row> rowIterator = sheet.iterator();
             Row row;
-            Cell cell;
-
-
             row = rowIterator.next();
-
             while (rowIterator.hasNext()) {
                 if (Util.getInstance().existInRow(row, "Nom") || Util.getInstance().existInRow(row, "Prénom") ||
                         Util.getInstance().existInRow(row, "Code PFE") || Util.getInstance().existInRow(row, "Thème du sujet") ||
@@ -94,52 +84,43 @@ public class InternshipFormatChecker implements ExcelFormat {
                             !Util.getInstance().existInRow(row, "Code PFE") || !Util.getInstance().existInRow(row, "Thème du sujet") ||
                             !Util.getInstance().existInRow(row, "Promoteur") || !Util.getInstance().existInRow(row, "Encadreur") ||
                             !Util.getInstance().existInRow(row, "Décision validation") ) {
-                        System.out.println("Missing one or more fields in the File, failed !");
+                        String str1="Champ(s) Introuvable(s) : ";
                         if (!Util.getInstance().existInRow(row, "Nom"))
-                            System.out.println("- Nom");
-                        if (!Util.getInstance().existInRow(row, "Prénom")) System.out.println("- Prénom");
-                        if (!Util.getInstance().existInRow(row, "Code PFE")) System.out.println("- Code PFE");
+                            str1=str1+ "\n- Nom";
+                        if (!Util.getInstance().existInRow(row, "Prénom")) str1=str1+ "\n- Prénom";
+                        if (!Util.getInstance().existInRow(row, "Code PFE")) str1=str1+ "\n- Code PFE";
                         if (!Util.getInstance().existInRow(row, "Thème du sujet"))
-                            System.out.println("- Thème du sujet");
+                            str1=str1+ "\n- Thème du sujet";
                         if (!Util.getInstance().existInRow(row, "Promoteur"))
-                            System.out.println("- Promoteur");
-                        if (!Util.getInstance().existInRow(row, "Encadreur")) System.out.println("- Encadreur");
-                        if (!Util.getInstance().existInRow(row, "Décision validation")) System.out.println("- Décision validation");
+                            str1=str1+ "\n- Promoteur";
+                        if (!Util.getInstance().existInRow(row, "Encadreur")) str1=str1+ "\n- Encadreur";
+                        if (!Util.getInstance().existInRow(row, "Décision validation")) str1=str1+ "\n- Décision validation";
                         f2.delete();
-                        return (false);
+                        throw new MissingFieldsException(str1);
 
-                    } else { System.out.println("File match !");
-                        f2.delete();
-                        return (true);
+                    } else {                        f2.delete();
+                        throw new FileAcceptedException("Fichier Accepté");
                     }
 
 
                 } else row = rowIterator.next();
 
-
             }
 
-
-        } catch (Exception ioe) {
-            ioe.printStackTrace();
-        }
-        System.out.println("Couldn't find target line !");
-        return(false);
+        throw new NoLineFoundException("Header de la table non-trouvé !");
     }
 
     @Override
-    public boolean checkDoublants(File f1) {
+    public boolean checkDoublants(String f1, int[] tab) throws FastInfosetException, IOException, Exception {
         return false;
     }
 
-    ;
 
-    public static void main(String[] args) {
-        File f1 = new File("LISTEDESSUJETSPFE20162017SIL.docx");
+    public static void main(String[] args) throws IOException, MissingFieldsException, NoLineFoundException, FileAcceptedException {
                 InternshipFormatChecker ssf = new InternshipFormatChecker();
        String y=ssf.ConvertWordTableToExcel("LISTEDESSUJETSPFE20162017SIL.docx");
        System.out.println(y);
-                 boolean b=ssf.checkFormat(f1);
+                 boolean b=ssf.checkFormat("LISTEDESSUJETSPFE20162017SIL.docx");
 
     }
 }
